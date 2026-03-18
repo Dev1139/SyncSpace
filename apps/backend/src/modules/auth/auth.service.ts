6e1dd9a1651b50095512d6329f987b78a19985ad
Dev1139 <devrajsinghkurmi2003@gmail.com>
2026-03-18 18:34:56 +0530

    feat: initial backend setup with auth, prisma, validation

---
+++ apps/backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {
+    console.log('AuthService created');
+  }
+
+  async register(email: string) {
+    try {
+      return await this.prisma.user.create({
+        data: {
+          email,
+        },
+      });
+    } catch (error) {
+      //Handle unique constraint
+      if ((error as any).code === 'P2002') {
+        throw new ConflictException('Email already exits');
+      }
+      throw error;
+    }
+  }
+}
